52aee8f1d081afc0287892bbd2ac97fd3a6d9273
Max <164526851+MaadMa4x@users.noreply.github.com>
2025-06-16 14:55:55 -0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,3 @@
+url = input("Digite um url: ")
+usuario = url.split("/")[-1]
+print (f"Usuario : {usuario}")
---
+++ 2.py
@@ -0,0 +1,3 @@
+comentario = "Houve um erro ao processo do pedido"
+posicao = comentario.find ("erro")
+print(f"Tamanho do comentário: {len(comentario)} caracteres")
